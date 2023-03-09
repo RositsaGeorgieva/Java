2a503400af9f6f69bb7635724a8863379939cc7f
Rositsa <rosy@inerty.com>
2023-03-09 12:51:34 +0200

    Hello world

---
+++ src/com/inerty/java/HelloWorld.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package com.inerty.java;
+
+/**
+ * HelloWorld
+ * 
+ * @author RG
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+
+	}
+
+}
